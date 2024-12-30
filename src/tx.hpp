c2e23e29743f664195123b529e0c1bfe7b801231
YOUR_NAME <YOUR_EMAIL>
2024-12-30 16:41:06 -0700

    tx #defines

--- src/tx.hpp
+++ src/tx.hpp
@@ -12,6 +12,9 @@ DIY Arduino based RC Transmitter
 #define CSN 10
 #define LOWV 3.0
 #define VDIV 2.0 // 1/n divider
+#define LED 7
+#define BATT A7
+#define POT A1
 
 RF24 radio(CE, CSN); // nRF24L01 (CE, CSN)
 byte pot = 1;
@@ -20,10 +23,10 @@ void setup()
 {
   Serial.begin(9600);
 
-  pinMode(7, OUTPUT); // Low Battery LED
-  digitalWrite(7, HIGH);
+  pinMode(LED, OUTPUT); // Low Battery LED
+  digitalWrite(LED, HIGH);
   delay(500); // Test LED on boot
-  digitalWrite(7, LOW);
+  digitalWrite(LED, LOW);
 
   radio.begin();
   radio.openWritingPipe(ADDRESS1);
@@ -36,11 +39,11 @@ void setup()
 void loop()
 {
   // Send data
-  pot = map(analogRead(A1), 0, 1023, 0, 255);
+  pot = map(analogRead(POT), 0, 1023, 0, 255);
   radio.write(&pot, sizeof(byte));
   // Check battery
-  float voltage = analogRead(A7) * (5.00 / 1023.00) * VDIV;
-  digitalWrite(7, voltage < LOWV); // HIGH if true
+  float voltage = analogRead(BATT) * (5.00 / 1023.00) * VDIV;
+  digitalWrite(LED, voltage < LOWV); // HIGH if true
   // Debug
   Serial.println(pot);
 }
